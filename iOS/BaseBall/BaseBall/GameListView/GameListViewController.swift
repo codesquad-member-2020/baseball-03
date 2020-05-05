e7c25e753d59d9b623637de3c804165e5cfb0e7e
1Consumption <hanseop95@gmail.com>
2020-05-06 00:44:16 +0900

    [#1] feat: 상단 게임 목록 레이블 추가.

--- iOS/BaseBall/BaseBall/GameListView/GameListViewController.swift
+++ iOS/BaseBall/BaseBall/GameListView/GameListViewController.swift
@@ -10,7 +10,28 @@ import UIKit
 
 class GameListViewController: UIViewController {
 
+    var gameListLabel: UILabel = {
+       let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 35)
+        label.textColor = .white
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSubview()
+        setupGameListLabel()
+    }
+    
+    private func setupSubview() {
+        self.view.addSubview(gameListLabel)
+    }
+    
+    private func setupGameListLabel() {
+        gameListLabel.translatesAutoresizingMaskIntoConstraints = false
+        gameListLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        gameListLabel.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.2).isActive = true
+        gameListLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        gameListLabel.text = "게임 목록"
     }
 }
