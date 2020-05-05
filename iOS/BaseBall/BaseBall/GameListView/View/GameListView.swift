e26671042e3a954c5cdcfb69f730b734c2f6d8f5
1Consumption <hanseop95@gmail.com>
2020-05-06 02:45:28 +0900

    [#1] feat: GameListView 커스텀 클래스 생성

---
+++ iOS/BaseBall/BaseBall/GameListView/View/GameListView.swift
@@ -0,0 +1,24 @@
+//
+//  GameListView.swift
+//  BaseBall
+//
+//  Created by 신한섭 on 2020/05/06.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class GameListView: UIView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.backgroundColor = .lightGray
+        self.alpha = 0.8
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .lightGray
+        self.alpha = 0.8
+    }
+}
