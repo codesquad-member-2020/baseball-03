b6e2dcdfef187ac7cf22dff5a2b760b1a25b3fda
1Consumption <hanseop95@gmail.com>
2020-05-05 18:50:55 +0900

    [#5] feat: 로고 버튼 클릭 시 GameListViewController로 present

---
+++ iOS/BaseBall/BaseBall/GameListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameListViewController.swift
+//  BaseBall
+//
+//  Created by 신한섭 on 2020/05/05.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class GameListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- iOS/BaseBall/BaseBall/LoginViewController.swift
+++ iOS/BaseBall/BaseBall/LoginViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class LoginViewController: UIViewController {
     private let logoName = ["Doosan", "Hanwha", "Kia", "Kiwoom", "KT", "LG", "Lotte", "NC", "Samsung", "SK"]
     
+    @IBAction func logoButtonPushed(_ sender: UIButton) {
+        guard let gameListViewController = storyboard?.instantiateViewController(identifier: "GameListViewController") else {return}
+        gameListViewController.modalPresentationStyle = .fullScreen
+        present(gameListViewController, animated: true)
+    }
+    
     @IBOutlet weak var logoButton: UIButton!
     
     override func viewDidLoad() {
@@ -23,14 +29,19 @@ class LoginViewController: UIViewController {
         self.logoButton.setImage(UIImage(named: name), for: .normal)
         self.view.backgroundColor = UIColor(named: name)
         
-        if name == "Hanwha" || name == "NC" {
+        if name == "Hanwha" || name == "NC" || name == "Kiwoom" {
             logoButton.translatesAutoresizingMaskIntoConstraints = false
             let widthAnchor = logoButton.widthAnchor
             logoButton.removeConstraints(logoButton.constraints)
             logoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
             logoButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
             logoButton.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-            logoButton.heightAnchor.constraint(equalTo: logoButton.widthAnchor, multiplier: 1 / 1.5).isActive = true
+            
+            if name == "Hanwha" {
+                logoButton.heightAnchor.constraint(equalTo: logoButton.widthAnchor, multiplier: 1 / 1.5).isActive = true
+            } else {
+                logoButton.heightAnchor.constraint(equalTo: logoButton.widthAnchor, multiplier: 1 / 1.4).isActive = true
+            }
         }
     }
 }
