5cb9a34b0da5c08493e1d5292da615852f904e0a
1Consumption <hanseop95@gmail.com>
2020-05-06 01:09:49 +0900

    [#1] feat: 게임리스트를 담을 스크롤뷰 생성

--- iOS/BaseBall/BaseBall/GameListView/GameListViewController.swift
+++ iOS/BaseBall/BaseBall/GameListView/GameListViewController.swift
@@ -10,28 +10,40 @@ import UIKit
 
 class GameListViewController: UIViewController {
 
-    var gameListLabel: UILabel = {
+    private var gameListLabel: UILabel = {
        let label = UILabel()
         label.font = .boldSystemFont(ofSize: 35)
         label.textColor = .white
         return label
     }()
     
+    private var scrollView: UIScrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubview()
         setupGameListLabel()
+        setupSrcollView()
     }
     
     private func setupSubview() {
         self.view.addSubview(gameListLabel)
+        self.view.addSubview(scrollView)
     }
     
     private func setupGameListLabel() {
         gameListLabel.translatesAutoresizingMaskIntoConstraints = false
         gameListLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        gameListLabel.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.2).isActive = true
+        gameListLabel.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2).isActive = true
         gameListLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         gameListLabel.text = "게임 목록"
+    }
+    
+    private func setupSrcollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: gameListLabel.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
 }
