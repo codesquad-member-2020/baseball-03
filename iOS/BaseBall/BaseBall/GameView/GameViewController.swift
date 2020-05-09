7d483be3804b93b97c307e8fb7cee37290988b58
1Consumption <hanseop95@gmail.com>
2020-05-09 12:25:56 +0900

    [#2] feat: 현재 투수/타자 표시

--- iOS/BaseBall/BaseBall/GameView/GameViewController.swift
+++ iOS/BaseBall/BaseBall/GameView/GameViewController.swift
@@ -8,9 +8,27 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "currentPlayerCell") as? CurrentPlayerTableViewCell else {return UITableViewCell()}
+        cell.accessoryView = UIImageView(image: UIImage(systemName: "checkmark"))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height / 2
+    }
+    
+    
+    @IBOutlet weak var currentPlayerTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentPlayerTableView.delegate = self
+        currentPlayerTableView.dataSource = self
     }
 }
---
+++ iOS/BaseBall/BaseBall/GameView/View/currentPlayerTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentPlayerTableViewCell.swift
+//  BaseBall
+//
+//  Created by 신한섭 on 2020/05/09.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class CurrentPlayerTableViewCell: UITableViewCell {
+}
