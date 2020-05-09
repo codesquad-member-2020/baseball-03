4441fd8b8daf7dec10e4acbc96c47e7f69a33bc5
1Consumption <hanseop95@gmail.com>
2020-05-10 01:00:05 +0900

    [#4] feat: 선수 상세 정보 테이블 뷰 추가

--- iOS/BaseBall/BaseBall/DetailScoreView/DetailScoreViewController.swift
+++ iOS/BaseBall/BaseBall/DetailScoreView/DetailScoreViewController.swift
@@ -9,15 +9,49 @@
 import UIKit
 
 class DetailScoreViewController: UIViewController {
-
+    
     @IBOutlet weak var teamSelectSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var playerInfoTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSegmentedControl()
+        playerInfoTableView.dataSource = self
+        playerInfoTableView.delegate = self
     }
     
     private func setupSegmentedControl() {
         teamSelectSegmentedControl.setTitleTextAttributes([.font : UIFont(name: "DungGeunMo", size: 17) as Any], for: .normal)
+    }
+}
+
+extension DetailScoreViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerInfoCell") as? PlayerInfoCell else {return UITableViewCell()}
+        if indexPath.row == 9 {
+            cell.configuateAverage()
+        } else {
+            cell.configurateCell()
+        }
+        return cell
+    }
+}
+
+extension DetailScoreViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return playerInfoTableView.frame.height / 11
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableCell(withIdentifier: "PlayerInfoCell") as? PlayerInfoCell else {return UIView()}
+        return header.contentView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return playerInfoTableView.frame.height / 11
     }
 }
