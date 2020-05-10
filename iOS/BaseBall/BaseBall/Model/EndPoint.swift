a114f218aa99a941644658b217e2bd6e48d9a340
1Consumption <hanseop95@gmail.com>
2020-05-10 20:43:13 +0900

    [#24] feat: EndPoint 정보를 관리할 enum 생성

---
+++ iOS/BaseBall/BaseBall/Model/EndPoint.swift
@@ -0,0 +1,15 @@
+//
+//  EndPoint.swift
+//  BaseBall
+//
+//  Created by 신한섭 on 2020/05/10.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+enum EndPoint {
+    static let defaultURL = "https://69612ad4-ca1b-4bc6-a7e2-a7d141c32cb5.mock.pstmn.io/api/"
+    static let teams = "teams"
+    static let matches = "matches"
+}
