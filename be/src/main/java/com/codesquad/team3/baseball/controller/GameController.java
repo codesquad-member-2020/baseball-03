16cad408caa1be9730145b9a1431d45aa78da174
beemiel <qnswwrn@gmail.com>
2020-05-12 19:57:50 +0900

    [#37] feat: 게임의 초기화 데이터를 넣는 메소드 매핑
    - 게임이 시작되면 현재 게임의 초기화 데이터를 halfInning, records,
    atBat에
    업데이트 해준다

--- be/src/main/java/com/codesquad/team3/baseball/controller/GameController.java
+++ be/src/main/java/com/codesquad/team3/baseball/controller/GameController.java
@@ -1,19 +1,31 @@
 package com.codesquad.team3.baseball.controller;
 
+import com.codesquad.team3.baseball.dto.PitchingDTO;
 import com.codesquad.team3.baseball.dto.ResponseData;
 import com.codesquad.team3.baseball.dto.Status;
+import com.codesquad.team3.baseball.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/games/{gameId}/teams/{teamId}")
 public class GameController {
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping("/init")
     public ResponseEntity<ResponseData> initGame(@PathVariable Integer gameId,
-                                                 @PathVariable Integer teamId) {
-        return new ResponseEntity<>(new ResponseData(Status.SUCCESS, null), HttpStatus.OK);
+                                                 @PathVariable Integer teamId,
+                                                 HttpSession session) {
+        gameService.initGame(gameId, session);
+        PitchingDTO pitchingDTO = gameService.getInitGameData(gameId, teamId);
+
+        return new ResponseEntity<>(new ResponseData(Status.SUCCESS, pitchingDTO), HttpStatus.OK);
     }
 
     @PostMapping("")
