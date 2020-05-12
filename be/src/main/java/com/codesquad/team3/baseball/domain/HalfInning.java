adaf2c93b8a21600fbb0543290b66daf15aeb762
beemiel <qnswwrn@gmail.com>
2020-05-12 18:23:31 +0900

    [#37] feat: 세션에 저장할 게임 진행 상황 class 추가

--- be/src/main/java/com/codesquad/team3/baseball/domain/HalfInning.java
+++ be/src/main/java/com/codesquad/team3/baseball/domain/HalfInning.java
@@ -1,4 +1,76 @@
 package com.codesquad.team3.baseball.domain;
 
 public class HalfInning {
+    private Integer id;
+    private int inning;
+    private boolean isTop;
+    private int score;
+    private int strike;
+    private int ball;
+    private int out;
+    private boolean first;
+    private boolean second;
+    private boolean third;
+
+    public HalfInning(Integer id, int inning, boolean isTop) {
+        this.id = id;
+        this.inning = inning;
+        this.isTop = isTop;
+    }
+
+    public int addScore(){
+        return this.score++;
+    }
+
+    public int addStrike(){
+        return this.strike++;
+    }
+
+    public int initStrike() {
+        return this.strike = 0;
+    }
+
+    public int addBall(){
+        return this.ball++;
+    }
+
+    public int initBall() {
+        return this.ball = 0;
+    }
+
+    public int addOut(){
+        return this.out++;
+    }
+
+    public int initOut() {
+        return this.out = 0;
+    }
+
+    public boolean turnFirst(boolean status) {
+        return this.first = status;
+    }
+
+    public boolean turnSecond(boolean status) {
+        return this.second = status;
+    }
+
+    public boolean turnThird(boolean status) {
+        return this.third = status;
+    }
+
+    @Override
+    public String toString() {
+        return "HalfInning{" +
+                "id=" + id +
+                ", inning=" + inning +
+                ", isTop=" + isTop +
+                ", score=" + score +
+                ", strike=" + strike +
+                ", ball=" + ball +
+                ", out=" + out +
+                ", first=" + first +
+                ", second=" + second +
+                ", third=" + third +
+                '}';
+    }
 }
