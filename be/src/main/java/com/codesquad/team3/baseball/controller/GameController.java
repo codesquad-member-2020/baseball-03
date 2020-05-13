6db7406c5a1aa887cec4850b6cfb70e210105d55
beemiel <qnswwrn@gmail.com>
2020-05-13 17:40:56 +0900

    [#37] feat: controller에 Exception Handler 메소드 작성
    중복키를 insert할 때 나는 예외를 처리하는 메소드 추가

--- be/src/main/java/com/codesquad/team3/baseball/controller/GameController.java
+++ be/src/main/java/com/codesquad/team3/baseball/controller/GameController.java
@@ -1,5 +1,6 @@
 package com.codesquad.team3.baseball.controller;
 
+import com.codesquad.team3.baseball.dto.ExceptionDTO;
 import com.codesquad.team3.baseball.dto.PitchingDTO;
 import com.codesquad.team3.baseball.dto.ResponseData;
 import com.codesquad.team3.baseball.dto.Status;
@@ -8,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/games/{gameId}/teams/{teamId}")
@@ -25,12 +30,19 @@ public class GameController {
         return new ResponseEntity<>(new ResponseData(Status.SUCCESS, pitchingDTO), HttpStatus.OK);
     }
 
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ExceptionDTO> duplicateKey() {
+        Map<String, String> content = new HashMap<>();
+        content.put("message", "이미 초기화된 게임입니다.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO("ERROR", content));
+    }
+
     @PostMapping("")
     public ResponseEntity<ResponseData> defend(@PathVariable Integer gameId,
                                                @PathVariable Integer teamId) {
         return new ResponseEntity<>(new ResponseData(Status.SUCCESS, null), HttpStatus.OK);
     }
-
+    
     @GetMapping("")
     public ResponseEntity<ResponseData> attack(@PathVariable Integer gameId,
                                                @PathVariable Integer teamId) {
