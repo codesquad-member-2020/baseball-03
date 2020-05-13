c34be9abd865c654daad1854615d183fbf1d045e
beemiel <qnswwrn@gmail.com>
2020-05-13 19:27:05 +0900

    [#37] feat: 게임 초기화 response DTO 변경 및 빌더 추가

--- be/src/main/java/com/codesquad/team3/baseball/dto/PitcherDTO.java
+++ be/src/main/java/com/codesquad/team3/baseball/dto/PitcherDTO.java
@@ -1,10 +1,13 @@
 package com.codesquad.team3.baseball.dto;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class PitcherDTO {
     private String name;
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     private int pitchCount;
 
     public PitcherDTO(String name) {
