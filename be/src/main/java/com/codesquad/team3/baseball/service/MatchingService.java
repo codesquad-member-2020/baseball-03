dfe7fee196d93f22cf21f4408186ed0b0aa70697
beemiel <qnswwrn@gmail.com>
2020-05-15 00:34:55 +0900

    [#33] feat: MatchingService에서 게임 선택 로직 작성
    - getSelectionResult(): 디비에서 데이터를 가져와서 반환한다
    - setTeamGame(): 디비에서 유저 id 유무를 가져와서 있으면 예외를 반환하고
                     없으면 디비에 유저의 id를 update한다
    		 이때 id 값들이 올바르지 않은 경우 예외처리 해준다

--- be/src/main/java/com/codesquad/team3/baseball/service/MatchingService.java
+++ be/src/main/java/com/codesquad/team3/baseball/service/MatchingService.java
@@ -3,6 +3,8 @@ package com.codesquad.team3.baseball.service;
 import com.codesquad.team3.baseball.dao.MatchingDAO;
 import com.codesquad.team3.baseball.dto.GameDTO;
 import com.codesquad.team3.baseball.dto.TeamDTO;
+import com.codesquad.team3.baseball.exception.CanNotSelectException;
+import com.codesquad.team3.baseball.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -26,5 +28,27 @@ public class MatchingService {
 
     public List<GameDTO> getAllGames() {
         return matchingDAO.selectAllTeamGames();
+    }
+
+    public Map<String, Boolean> getSelectionResult(Integer gameId, Integer teamId) {
+        Map<String,Boolean> result = new HashMap<>();
+        result.put("isHome", setTeamGame(gameId, teamId));
+        result.put("isReady", true);
+        return result;
+    }
+
+
+    private boolean setTeamGame(Integer gameId, Integer teamId) {
+        Map<String,Object> matchData = matchingDAO.getMatchDataWithGameIdAndTeamId(gameId,teamId);
+
+        if(matchData.isEmpty()) {
+            throw new NotFoundException();
+        }
+
+        if(matchData.get("user") != null) {
+            throw new CanNotSelectException();
+        }
+
+        return (Boolean)matchData.get("is_home");
     }
 }
