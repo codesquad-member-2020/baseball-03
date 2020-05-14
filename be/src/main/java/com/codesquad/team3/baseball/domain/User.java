19de64eda32d73bfcc65d959a8a646339b19dac8
beemiel <qnswwrn@gmail.com>
2020-05-14 21:18:44 +0900

    [#37] feat: User 도메인 생성

---
+++ be/src/main/java/com/codesquad/team3/baseball/domain/User.java
@@ -0,0 +1,41 @@
+package com.codesquad.team3.baseball.domain;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+    @Id
+    private Integer id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private int win;
+
+    private int lose;
+
+    private int draw;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public boolean matchPassword(String password) {
+        return this.password.equals(password);
+    }
+
+}
