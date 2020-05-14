86acbaddd579a1bce4c40e857f598bff62a07b81
beemiel <qnswwrn@gmail.com>
2020-05-15 00:38:09 +0900

    [#33] feat: MatchingController에 게임 선택 메소드 추가
    - 팀 선택이되면 200과 매치 여부를 반환한다
    - 선택 후 매칭을 기다리는 메소드 추가
    - 로그인 예외처리 추가
    - 존재하지 않는 id 예외처리 추가
    - 이미 선택된 팀 예외처리 추가

--- be/src/main/java/com/codesquad/team3/baseball/controller/MatchingController.java
+++ be/src/main/java/com/codesquad/team3/baseball/controller/MatchingController.java
@@ -1,11 +1,19 @@
 package com.codesquad.team3.baseball.controller;
 
+import com.codesquad.team3.baseball.dto.ExceptionDTO;
 import com.codesquad.team3.baseball.dto.ResponseData;
 import com.codesquad.team3.baseball.dto.Status;
+import com.codesquad.team3.baseball.exception.CanNotSelectException;
+import com.codesquad.team3.baseball.exception.NonLoginException;
+import com.codesquad.team3.baseball.exception.NotFoundException;
 import com.codesquad.team3.baseball.service.MatchingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -30,7 +38,43 @@ public class MatchingController {
 
     @PatchMapping("/games/{gameId}/teams/{teamId}")
     public ResponseEntity<ResponseData> selectGame(@PathVariable int gameId,
-                                                   @PathVariable int teamId) {
+                                                   @PathVariable int teamId,
+                                                   HttpSession session) {
+        if(!checkLogin(session)) {
+            throw new NonLoginException();
+        }
+
+        Map<String, Boolean> content = matchingService.getSelectionResult(gameId, teamId);
+        return new ResponseEntity<>(new ResponseData(Status.SUCCESS, content), HttpStatus.OK);
+    }
+
+    @GetMapping("/games/{gameId}/teams/{teamId}/matching")
+    public ResponseEntity<ResponseData> selectGame() {
         return new ResponseEntity<>(new ResponseData(Status.SUCCESS, null), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(NonLoginException.class)
+    public ResponseEntity<ExceptionDTO> cannotLogin() {
+        Map<String, String> content = new HashMap<>();
+        content.put("message", "로그인 해주세요.");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ExceptionDTO("ERROR", content));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionDTO> notFound() {
+        Map<String, String> content = new HashMap<>();
+        content.put("message", "게임 혹은 팀을 찾을 수 없습니다.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO("ERROR", content));
+    }
+
+    @ExceptionHandler(CanNotSelectException.class)
+    public ResponseEntity<ExceptionDTO> catNotSelect() {
+        Map<String, String> content = new HashMap<>();
+        content.put("message", "이미 선택된 팀입니다.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO("ERROR", content));
+    }
+
+    private boolean checkLogin(HttpSession session) {
+        return session.getAttribute("user") != null;
     }
 }
