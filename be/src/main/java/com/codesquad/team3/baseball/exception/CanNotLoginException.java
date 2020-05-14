cf24d8e37c6de90b7314fd1880806113e54e0754
beemiel <qnswwrn@gmail.com>
2020-05-14 21:20:32 +0900

    [#47] feat: 로그인 관련 예외 추가
    - CanNotLoginException: 중복 로그인
    - NotFoundException: 존재하지 않는 회원
    - NotMatchPassword: 비밀번호 불일치

---
+++ be/src/main/java/com/codesquad/team3/baseball/exception/CanNotLoginException.java
@@ -0,0 +1,5 @@
+package com.codesquad.team3.baseball.exception;
+
+public class CanNotLoginException extends RuntimeException {
+    private String message;
+}
---
+++ be/src/main/java/com/codesquad/team3/baseball/exception/NotFoundException.java
@@ -0,0 +1,6 @@
+package com.codesquad.team3.baseball.exception;
+
+public class NotFoundException extends RuntimeException {
+    private String message;
+
+}
