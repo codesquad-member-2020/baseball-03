05c59631714a1f999739673388d093d0e7feba74
Jay <beginin15@gmail.com>
2020-05-15 11:36:00 +0900

    [#42] refactor: 선수가 교체된 경우, 공수가 교체된 경우 예외 처리
    - InGameDAO: findLastAtBat - 가장 최근 게임 로그를 기준으로 하면 새로운 타석으로 교체된 데이터를 보여줄 수 없음, isManyAtBatsInHalfInning - 인자로 전달된 atBat의 id값과 연결된 하프이닝의 개수를 구하여 새로운 하프 이닝인지 판단. (단 최초 실행 시에는 하프 이닝이 1개이므로 걸러내지 못함)
    - AtBat: 동일한 타석인지 판단하는 equals 정의
    - InGameService: 가장 최근 타석과 하프 이닝을 조회하여 DTO를 구성한다. 공수가 바뀐 경우를 고려하기 위해 조건식이 추가 됨

--- be/src/main/java/com/codesquad/team3/baseball/dao/InGameDAO.java
+++ be/src/main/java/com/codesquad/team3/baseball/dao/InGameDAO.java
@@ -6,12 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -162,6 +165,30 @@ public class InGameDAO {
                         Result.valueOf(rs.getString("result")),
                         rs.getTimestamp("create_time").toLocalDateTime(),
                         rs.getInt("at_bat")));
+    }
+
+    public AtBat findLastAtBat(Integer gameId) {
+        String sql = "SELECT ab.id AS at_bat_id, ab.half_inning, ab.hitter, ab.strikes, ab.balls" +
+                " FROM at_bat ab" +
+                " LEFT OUTER JOIN half_inning hi ON ab.half_inning = hi.id" +
+                " WHERE hi.game = :gameId" +
+                " ORDER BY ab.id DESC" +
+                " LIMIT 1";
+        return namedParameterJdbcTemplate.queryForObject(sql, new MapSqlParameterSource("gameId", gameId),
+                (rs, rowNum) -> new AtBat(
+                        rs.getInt("at_bat_id"),
+                        rs.getInt("half_inning"),
+                        rs.getInt("hitter"),
+                        rs.getInt("strikes"),
+                        rs.getInt("balls")));
+    }
+
+    public boolean isManyAtBatsInHalfInning(Integer atBatId) {
+        String sql = "SELECT COUNT(half_inning) > 1" +
+                " FROM at_bat ab" +
+                " WHERE ab.half_inning = (SELECT half_inning" +
+                " FROM at_bat WHERE id = :atBatId)";
+        return namedParameterJdbcTemplate.queryForObject(sql, new MapSqlParameterSource("atBatId", atBatId), Boolean.class);
     }
 
     public void addGameLog(GameLog log) {
